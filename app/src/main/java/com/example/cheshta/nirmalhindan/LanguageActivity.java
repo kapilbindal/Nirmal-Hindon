957aaaf5e3e52cf0eecc83ce67068ed514e622f5
Kapil Bindal <kapil.bindal98@gmail.com>
2018-04-06 19:17:32 +0530

    Login and register activity with OTP addded

--- app/src/main/java/com/example/cheshta/nirmalhindan/LanguageActivity.java
+++ app/src/main/java/com/example/cheshta/nirmalhindan/LanguageActivity.java
@@ -1,13 +1,24 @@
 package com.example.cheshta.nirmalhindan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LanguageActivity extends AppCompatActivity {
+    Button btnEnglish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_language);
+        btnEnglish = findViewById(R.id.btnEnglish);
+        btnEnglish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LanguageActivity.this,LoginActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/cheshta/nirmalhindan/LoginActivity.java
@@ -0,0 +1,31 @@
+package com.example.cheshta.nirmalhindan;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button btnLogin,btnCreate;
+    EditText edMobNum,edPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btnCreate = findViewById(R.id.btnCreate);
+        btnLogin = findViewById(R.id.btnLogin);
+        edMobNum =  findViewById(R.id.edMobNum);
+        edPassword = findViewById(R.id.edPassword);
+
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
+    }
+}
