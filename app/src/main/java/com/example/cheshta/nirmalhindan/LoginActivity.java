e4de9c5389d4db1d917d9f00a23d05b06f29bb6e
Kapil Bindal <kapil.bindal98@gmail.com>
2018-04-06 23:57:18 +0530

    num with pwd complete

--- app/src/main/java/com/example/cheshta/nirmalhindan/LoginActivity.java
+++ app/src/main/java/com/example/cheshta/nirmalhindan/LoginActivity.java
@@ -1,16 +1,29 @@
 package com.example.cheshta.nirmalhindan;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button btnLogin,btnCreate;
     EditText edMobNum,edPassword;
+    TextView tvForgotPassword;
+    private FirebaseAuth mAuth;
+    ProgressDialog progressDialog;
+    String mobNumberLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +33,54 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin = findViewById(R.id.btnLogin);
         edMobNum =  findViewById(R.id.edMobNum);
         edPassword = findViewById(R.id.edPassword);
+        tvForgotPassword = findViewById(R.id.tvForgotPassword);
+
+        mobNumberLogin = "+91".concat(edMobNum.getText().toString());
+
+        mAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Logging In");
+        progressDialog.setMessage("Please wait while we check your credentials.");
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(!edMobNum.getText().toString().isEmpty() && !edPassword.getText().toString().isEmpty()){
+                    progressDialog.show();
+                    mAuth.signInWithEmailAndPassword(mobNumberLogin.concat("@nirmal.com"),edPassword.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    progressDialog.dismiss();
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(LoginActivity.this, "Login Successfull", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(LoginActivity.this,HomeActivity.class));
+                                    }
+                                    else {
+                                        Toast.makeText(LoginActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+                else {
+                    Toast.makeText(LoginActivity.this, "Enter Email and Password!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         btnCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
+
+        tvForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LoginActivity.this, "Work In Progress!!", Toast.LENGTH_SHORT).show();
             }
         });
     }
