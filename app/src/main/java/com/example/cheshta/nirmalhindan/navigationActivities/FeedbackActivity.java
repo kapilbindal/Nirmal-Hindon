683cd6e2b8d9328316d7816f87b508138f16b5e6
Kapil Bindal <kapil.bindal98@gmail.com>
2018-04-07 14:27:46 +0530

    Contents added

--- app/src/main/java/com/example/cheshta/nirmalhindan/navigationActivities/FeedbackActivity.java
+++ app/src/main/java/com/example/cheshta/nirmalhindan/navigationActivities/FeedbackActivity.java
@@ -1,11 +1,16 @@
 package com.example.cheshta.nirmalhindan.navigationActivities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.cheshta.nirmalhindan.HomeActivity;
 import com.example.cheshta.nirmalhindan.R;
 import com.example.cheshta.nirmalhindan.utils.BottomNavigationViewHelper;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
@@ -19,6 +24,25 @@ public class FeedbackActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+        Button b1,b2;
+        b1=findViewById(R.id.fb1);
+        b2=findViewById(R.id.fb2);
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(FeedbackActivity.this,HomeActivity.class);
+                startActivity(i);
+            }
+        });
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FeedbackActivity.this, "Your response has been submitted "+"          THANK YOU!!", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(FeedbackActivity.this,HomeActivity.class);
+                startActivity(i);
+            }
+        });
 
         Log.d(TAG, "onCreate: Feedback");
 
